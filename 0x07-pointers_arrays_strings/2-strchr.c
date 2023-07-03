133c6324cb83cea7654a180cd8c4ad6a0327dd0b
MohamedHdd92 <memoun.haddad@gmail.com>
2023-07-03 08:13:09 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,12 @@ char *_strchr(char *s, char c)
 {
 	int itr;
 
-	for (itr = 0; s[itr] > = '/0'; itr++)
+	for (itr = 0; s[itr] >= '/0'; itr++)
 	{
 		if (s[itr] == c)
 		{
 			return (s + itr);
 		}
 	}
-	return (0);
+	return ('/0');
 }
