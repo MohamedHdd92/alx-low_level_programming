74e11f28e79beff0ef37bf18ef8408ef5bdd9c03
MohamedHdd92 <memoun.haddad@gmail.com>
2023-07-03 08:59:38 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,13 +14,15 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (r = 0; accept[r]; r++)
 		{
-			n++;
-			break;
-		}
-		else if (accept[r + 1] == '\0')
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
 			return (n);
+		}
+		s++;
 	}
-	s++;
-}
-return (n);
+	return (n);
 }
