000017d4b9051c282911a9e88f23fc623807cbbc
MohamedHdd92 <memoun.haddad@gmail.com>
2023-07-18 15:54:35 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -83,5 +83,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = _strcpy(dog->name, name);
 	dog->owner = _strcpy(dog->owner, owner);
 	dog->age = age;
-	return (dog);
+	return (new_dog);
 }
